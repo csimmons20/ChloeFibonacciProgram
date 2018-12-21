ca82214047db9d2766673ec4be0d6af81be8d20d
csimmons20 <csimmons20@scienceleadership.org>
2018-12-21 13:56:38 -0500

    Working on it.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,23 +3,38 @@ public class Fibonacci implements Runnable{
        int FN;
        int SN;
        int TN;
+       boolean recursiveMode;
 
 
-    Fibonacci(int NumberOne, int NumberTwo, int NumberThree) {
+    Fibonacci(int NumberOne, int NumberTwo, int NumberThree, boolean mode) {
         FN = NumberOne;
         SN = NumberTwo;
         TN = NumberThree;
+        mode = recursiveMode;
     }
+
+    private void RecursiveMode(int i){
+        if (i > 0){
+           RecursiveMode(i - 1);
+        }
+    }
+    public void InterativeMode(){
+        System.out.println(FN);
+        System.out.println(SN);
+        for (int i = TN - 2; i > 0; i--) {
+            System.out.println(FN + SN);
+            FN = FN + SN;
+        }
+    }
+
         public void run() {
-            System.out.println(FN);
-            System.out.println(SN);
-            for (int i = TN - 2; i > 0; i--) {
-                System.out.println(FN + SN);
-                FN = FN + SN;
-            }
-
-
+           if (recursiveMode = true){
+               RecursiveMode();
+           }
 
         }
 
+    private void RecursiveMode() {
     }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 5; i++) {
-            Runnable myFibonacci = new Fibonacci(2,5,4);
-            Runnable myFibonacci2 = new Fibonacci(1,2,6);
+        for(int i = 0; i < 1; i++) {
+            Runnable myFibonacci = new Fibonacci(2,5,4, true);
+            Runnable myFibonacci2 = new Fibonacci(1,2,4, true);
             Thread myThread = new Thread(myFibonacci);
             Thread myThread2 = new Thread(myFibonacci2);
             myThread.start();
             myThread2.start();
-            myFibonacci.run();
+            //myFibonacci.run();
             // 2 5 7 12
             System.out.println();
         }
